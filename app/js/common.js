40569ff131db58373f1b0f4ac6e475838d1d7044
alex <alex_kobilianskiy@bigmir.net>
2020-08-25 17:58:16 +0300

    header & sidebar (without adaptation)

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
+    var element = document.querySelector( '.main-mnu' );
+
+    var droppy = new Droppy( element, {
+        parentSelector: 'li',
+        dropdownSelector: 'li > ul',
+        triggerSelector: 'a'
+    } );
+
+    $('.droppy__parent').on("mouseenter", function(){
+        console.log("s")
+        $(this).children('.droppy__drop ').addClass('droppy__drop--active')
+    });
+
+    $('.droppy__parent').on("mouseleave", function(){
+        console.log("s")
+        $(this).children('.droppy__drop ').removeClass('droppy__drop--active')
+    });
+
+
+
     //E-mail Ajax Send
     $("form").submit(function() { //Change
         var th = $(this);
